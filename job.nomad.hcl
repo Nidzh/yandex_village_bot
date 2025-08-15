3e6cd45736c55155d1fb5a85e9444958f735b257
Nidzhat Ragimov <n.ragimov@outlook.com>
2025-08-15 14:27:43 +0400

    Update version to 0.0.8 in Nomad configuration, adjust transfer details, and reorganize media files

--- job.nomad.hcl
+++ job.nomad.hcl
@@ -2,7 +2,7 @@ job "yandex_village_bot_back" {
   datacenters = ["dc1"]
   type = "service"
   meta = {
-    "version" = "0.0.7"
+    "version" = "0.0.8"
   }
 
   group "yandex_village_bot_back-group" {
--- src/tgbot/handlers/transfer.py
+++ src/tgbot/handlers/transfer.py
@@ -11,7 +11,7 @@ router = Router()
 async def transfer(callback, state, text, media):
     title = (
         f"🚌 {_B('Дорогие друзья!')} Добраться до Open Air Яндекс Вертикалей можно на комфортных автобусах.\n\n"
-        f"📍 Место отбытия — ул. Зеленоградская, 41 (Парковочная зона 4098).\n\n"
+        f"📍 Место отбытия — Метро “Ховрино” Автомобильная парковка у выхода №4.\n\n"
         f"⏰ {_B('Время отбытия:')}\n"
         f"12:30 — 1 рейс\n"
         f"13:00 — 2 рейс\n"
@@ -20,8 +20,8 @@ async def transfer(callback, state, text, media):
     )
 
     kb = InlineKeyboardBuilder()
-    kb.button(text="📍 Я.Карты", url="https://yandex.com.ge/maps/-/CHtFuH6M")
-    kb.button(text="🔍 Высокое разрешение", url="https://clck.ru/3Nfkyk")
+    kb.button(text="📍 Я.Карты", url="https://yandex.ru/maps/-/CHtbR2kH")
+    kb.button(text="🔍 Высокое разрешение", url="https://clck.ru/3Ng96j")
     kb.button(text=text.back, callback_data="main")
     kb.adjust(1)
 
