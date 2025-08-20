95dfa71d6a25ed1c2f13d189c849322b14b36883
Nidzhat Ragimov <n.ragimov@outlook.com>
2025-08-20 12:09:44 +0400

    Add 20-08 specific scheduled jobs, update media mappings, reorganize associated media files, and update version to 0.0.12 in Nomad configuration

--- job.nomad.hcl
+++ job.nomad.hcl
@@ -2,7 +2,7 @@ job "yandex_village_bot_back" {
   datacenters = ["dc1"]
   type = "service"
   meta = {
-    "version" = "0.0.11"
+    "version" = "0.0.12"
   }
 
   group "yandex_village_bot_back-group" {
--- src/scheduler/app.py
+++ src/scheduler/app.py
@@ -4,18 +4,18 @@ from zoneinfo import ZoneInfo
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 
-from src.scheduler.groups.moscow import day_19_08
+from src.scheduler.groups.moscow import day_20_08
 
 scheduler = AsyncIOScheduler(timezone=UTC)
 
 async def run_test():
-    # await day_19_08.time_13_00(test=True)
-    # await day_19_08.time_15_00(test=True)
-    # await day_19_08.time_17_00(test=True)
-    # await day_19_08.time_18_00(test=True)
+    await day_20_08.time_12_00(test=True)
+    await day_20_08.time_14_00(test=True)
+    await day_20_08.time_16_00(test=True)
+    await day_20_08.time_18_00(test=True)
     ...
 
-scheduler.add_job(day_19_08.time_13_00, CronTrigger(day=19, hour=13, minute=00, timezone=ZoneInfo("Europe/Moscow")))
-scheduler.add_job(day_19_08.time_15_00, CronTrigger(day=19, hour=15, minute=00, timezone=ZoneInfo("Europe/Moscow")))
-scheduler.add_job(day_19_08.time_17_00, CronTrigger(day=19, hour=17, minute=00, timezone=ZoneInfo("Europe/Moscow")))
-scheduler.add_job(day_19_08.time_18_00, CronTrigger(day=19, hour=18, minute=00, timezone=ZoneInfo("Europe/Moscow")))
+scheduler.add_job(day_20_08.time_12_00, CronTrigger(day=20, hour=12, minute=00, timezone=ZoneInfo("Europe/Moscow")))
+scheduler.add_job(day_20_08.time_14_00, CronTrigger(day=20, hour=14, minute=00, timezone=ZoneInfo("Europe/Moscow")))
+scheduler.add_job(day_20_08.time_16_00, CronTrigger(day=20, hour=16, minute=00, timezone=ZoneInfo("Europe/Moscow")))
+scheduler.add_job(day_20_08.time_18_00, CronTrigger(day=20, hour=18, minute=00, timezone=ZoneInfo("Europe/Moscow")))
