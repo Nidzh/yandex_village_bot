ba25d33e0c62f808d06be16d4a1c08d99d269f39
Nidzhat Ragimov <n.ragimov@outlook.com>
2025-08-21 13:49:54 +0400

    Update 21-08 jobs, enhance "answer" functionality, adjust button texts, refactor state handling, update media mappings, and bump version to 0.0.15 in Nomad configuration

--- src/scheduler/app.py
+++ src/scheduler/app.py
@@ -9,11 +9,11 @@ from src.scheduler.groups.moscow import day_21_08
 scheduler = AsyncIOScheduler(timezone=UTC)
 
 async def run_test():
-    # await day_21_08.time_12_00(test=True)
-    # await day_21_08.time_16_00(test=True)
-    # await day_21_08.time_18_00(test=True)
+    await day_21_08.time_12_00(test=True)
+    await day_21_08.time_16_00(test=True)
+    await day_21_08.time_18_00(test=True)
     ...
 
 scheduler.add_job(day_21_08.time_12_00, CronTrigger(day=21, hour=12, minute=5, timezone=ZoneInfo("Europe/Moscow")))
-# scheduler.add_job(day_21_08.time_16_00, CronTrigger(day=21, hour=16, minute=00, timezone=ZoneInfo("Europe/Moscow")))
-# scheduler.add_job(day_21_08.time_18_00, CronTrigger(day=21, hour=18, minute=00, timezone=ZoneInfo("Europe/Moscow")))
+scheduler.add_job(day_21_08.time_16_00, CronTrigger(day=21, hour=16, minute=00, timezone=ZoneInfo("Europe/Moscow")))
+scheduler.add_job(day_21_08.time_18_00, CronTrigger(day=21, hour=18, minute=00, timezone=ZoneInfo("Europe/Moscow")))
--- src/tgbot/handlers/answer.py
+++ src/tgbot/handlers/answer.py
@@ -2,6 +2,9 @@ from aiogram import F, Router
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.fsm.state import State, StatesGroup
 
+from src.apps.user.schemas import AnswerCreate
+from src.apps.user.service import UserService
+from src.db.context import get_session
 from src.tgbot.formatter import _B
 from src.tgbot.utils import smart_edit
 
@@ -9,16 +12,56 @@ router = Router()
 
 
 
-class FeedbackState(StatesGroup):
-    ANSWER_FAIRTALE = State()
-    ANSWER_COCKTAIL = State()
+class AnswerState(StatesGroup):
+    FAIRYTALE = State()
+    COCKTAIL = State()
 
 
-@router.callback_query(F.data == "answer_fairytale")
+@router.callback_query(F.data.startswith('answer:'))
 async def answer_fairytale(callback, state, text, media):
-    title = f"🎲 {_B('Кот Учёный')} слушает внимательно!\n\n✍️ Напиши все ответы одним сообщением 👇"
+    await callback.answer()
+
+    data = callback.data.split(':')
+    question_code = data[1]
+    await state.set_data({"question_code": question_code})
+
+    match question_code:
+        case "fairytale":
+            await state.set_state(AnswerState.FAIRYTALE)
+            title = (
+                f"😺 {_B('Кот Учёный')} навострил уши!\n\n"
+                "✍️ Напиши все свои ответы одним сообщением:"
+            )
+        case "cocktail":
+            await state.set_state(AnswerState.COCKTAIL)
+            title = (
+                f"😺 {_B('Кот Учёный')} ждёт твой креатив!\n\n"
+                "✍️ Пришли рецепт целиком в одном сообщении:"
+            )
 
 
-@router.callback_query(F.data == "answer_cocktails")
-async def answer_cocktails(callback, state, text, media):
-    ...
+    await callback.message.answer(title)
+
+
+@router.message(AnswerState.FAIRYTALE)
+@router.message(AnswerState.COCKTAIL)
+async def admin_answer(message, state):
+    data = await state.get_data()
+    question_code = data['question_code']
+
+    if not message.text:
+        await message.answer("❌ Ответ должен быть текстом.")
+        return
+
+    async for session in get_session():
+        await UserService(session=session).create_answer(
+            AnswerCreate(
+                user_id=message.from_user.id,
+                question_code=question_code,
+                answer=message.text,
+            )
+        )
+
+    await message.answer("📜 Ответ записан в летопись!")
+    await state.clear()
+
